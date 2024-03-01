c155c473a7af155d7906736ac4970e2c7d13ccf1
apiscriptotomatis <159345943+apiscriptotomatis@users.noreply.github.com>
2024-03-02 00:34:47 +0700

    Create lifeafter.js

---
+++ lifeafter.js
@@ -0,0 +1,27 @@
+var api = '9fcd9b5c9e6d885209e09b4b3efd598e';
+    game = "lifeafter"
+function cekNick() {
+   var id = $('#id').val();
+   var zone = $('#zone').val();
+   console.log('ID:', id);
+   console.log('Zone:', zone);
+   if (id === "") {
+    $('#nick').text('');
+    $('#emptyInputToast').toast('show');
+    return;
+   }
+   $.ajax({
+    method: "GET",
+    url: "https://api-otomatis.my.id/trueid/games/" + game + "/?id=" + id + "&zone=" + zone + "&key=" + api,
+        beforeSend: function () {
+     $('#nick').text('Sedang Mengecek...');
+    },
+    success: function (response) {
+     if (response.hasOwnProperty('error_msg')) {
+      $('#nick').text('Tidak Ditemukan!');
+     } else {
+      $('#nick').text(response.nickname);
+     }
+    }
+   });
+}
